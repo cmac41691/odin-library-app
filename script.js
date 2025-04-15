297e366ee8e8e9202fe12deac8c755c128b54f52
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-04-15 08:48:58 -0400

    Build base structure: HTML, CSS, Book constructor

---
+++ script.js
@@ -0,0 +1,48 @@
+  
+
+
+
+// Book Contructors 
+function Book(title, author, pages, hasBeenRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasBeenRead = hasBeenRead;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
