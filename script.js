e9aba7ea54edf327b873ad6d4776001d589ae8af
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-04-17 07:53:00 -0400

    Add from open/close logic and event listeners

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-  
+// event listener to either create a new book and add it  
+const addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", addBookToLibrary);
 
+const newBookBtn = document.querySelector("#newBtn");
+newBookBtn.addEventListener("click", () => popUpform.style.display = "block");
 
+const popUpForm = document.getElementById("popUp")
+const closePopUp = document.querySelector(".close");
 
+closePopUp.addEventListener("click", () => {
+   popUpForm.style.display = "none"
+});
 // Book Contructors 
-function Book(title, author, pages, hasBeenRead){
+function Book(title, author, pages, hasBeenRead){  
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasBeenRead = hasBeenRead;
 }
+// These variables create, and adds to the library
+let mylibrary = [];
+let newBook;
+
+// This Fuction to add book contructors to the library
+function addBookToLibrary() {
+   
+} 
 
 
-
-
-
-
-
-
-
+Book.prototype.bookData = function () {
+    console.log(
+      "Author: " + this.author +
+      "\nTitle: " + this.title +
+      "\nPages: " + this.pages +
+      "\nDid you read any of these?: " + (this.hasBeenRead ? "Yes" : "No")
+    );
+  };
+  
+  Book.prototype.toggleRead = function () {
+    this.hasBeenRead = !this.hasBeenRead;
+  }; 
+  
 
 
 
