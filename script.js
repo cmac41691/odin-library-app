1d50b39fe8b8cfcce72abae6a9bcdf0cdf2fa04d
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-04-18 07:57:04 -0400

    Add DOM rendering of book cards to library container

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ mylibrary.push(newBook);
 newBook.bookData();
 
 document.getElementById("form").reset();
-popUpForm.style.display = "none"
+popUpForm.style.display = "none"  
 } 
 
 Book.prototype.bookData = function () {
@@ -52,7 +52,41 @@ Book.prototype.bookData = function () {
     this.hasBeenRead = !this.hasBeenRead; 
   }; 
   
+// render Library function
+function renderLibary() {
+  const container = document.getElementById("library-container");
+  container.innerHTML = ""; // This will clear the container first
 
+  mylibrary.forEach((book, index) => {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+
+    const author = document.createElement("h3")
+    author.textContent = "Author:" + book.author;
+
+    const pages = document.createElement("p");
+    pages.textContent = "Pages: " + book.pages;
+
+    const read = document.createElement("p");
+    read.textContent = "Read: " +(book.hasBeenRead ? "Yes" : "No");
+
+  // Append these elements to the card  
+card.appendChild(title);
+card.appendChild(author);
+card.appendChild(pages);
+card.appendChild(read);
+
+// Append card to the container
+container.appendChild(card);
+
+// Shows the Library  
+renderLibary();
+  });
+  
+}
 
 
 
